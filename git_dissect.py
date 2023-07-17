0b11c8f144fa928063363c59378e72cef94aaf59
Brendan Jackman <bhenryj0117@gmail.com>
2023-07-17 12:21:55 +0200

    no tests case

--- git_dissect.py
+++ git_dissect.py
@@ -16,20 +16,32 @@ class NotBisectingError(Exception):
 class CalledProcessError(Exception):
     pass
 
-def run_cmd(args):
+def run_cmd(args: list[str]) -> str:
     result = subprocess.run(args, capture_output=True)
     # subprocess.CompletedProcess.check_returncode doesn't set stderr.
     if result.returncode != 0:
         raise CalledProcessError(
             f'Command {args} failed with code {result.returncode}. ' +
             f'Stderr:\n{result.stderr.decode()}')
-    return result.stdout
+    return result.stdout.decode()
+
+def bisect_revlist() -> list[str]:
+    all_refs = (l.split(" ")[1] for l in run_cmd(["git", "show-ref"]).splitlines())
+    good_refs = (r for r in all_refs if r.startswith("refs/bisect/good"))
+    args =  ["git", "rev-list", "--bisect-all", "refs/bisect/bad"]
+    args += ["^" + r for r in good_refs]
+    return [l.split(" ")[0] for l in run_cmd(args).splitlines()]
 
 def dissect(args):
     try:
         run_cmd(["git", "bisect", "log"])
     except CalledProcessError:
         raise NotBisectingError("Couldn't run 'git bisect log' - did you run 'git bisect'?")
+
+    revs = bisect_revlist()
+    if len(revs) == 1:
+        return revs[0]
+    print(revs)
 
 if __name__ == "__main__":
     print(run_cmd(["cat", "/etc/shadow"]))
--- git_dissect_test.py
+++ git_dissect_test.py
@@ -4,6 +4,7 @@ import unittest
 import os
 import shutil
 import subprocess
+import sys
 import tempfile
 
 import git_dissect
@@ -14,33 +15,51 @@ class TestGitDissect(unittest.TestCase):
         self.addCleanup(lambda: shutil.rmtree(tmpdir))
         os.chdir(tmpdir)
 
-        self.git(["init"])
+        self.git("init")
 
-    def git(self, args):
-        res = subprocess.run(["git"] + args, capture_output=True)
-        res.check_returncode()
-        return res.stdout
+    def git(self, *args):
+        res = subprocess.run(("git",) + args, capture_output=True)
+        try:
+            res.check_returncode()
+        except:
+            print(f"Git command {args} failed.")
+            print(res.stderr.decode())
+            raise
+        return res.stdout.decode().strip()
 
     def commit(self, msg="dummy commit message"):
-        self.git(["commit", "-m", msg])
-        return self.git(["rev-list", "HEAD"])
+        self.git("commit", "-m", msg)
+        return self.git("rev-parse", "HEAD")
 
     def write(self, path, content):
         with open(path, "w") as f:
             f.write(content)
-        self.git(["add", path])
+        self.git("add", path)
 
     def test_not_bisect(self):
         with self.assertRaises(git_dissect.NotBisectingError):
             git_dissect.dissect([])
 
-    def test_smoke(self):
+    def test_no_tests_needed(self):
         self.write("run.sh", "true")
         good = self.commit()
         self.write("run.sh", "exit 1")
         bad = self.commit()
 
-        self.git(["bisect", "start"])
+        self.git("bisect", "start")
+        self.git("bisect", "good", good)
+        self.git("biset", "bad", bad)
+
+        self.assertEqual(git_dissect.dissect(["run.sh"]), bad, f"good: {good} bad: {bad}")
+
+    # TODO:
+    #  linear case
+    #  nonlinear, multiple good
+    #  nonlinear, single good
+    #  worktree mode
+    #  non-worktree mode
+    #  replacing args
+
 
 if __name__ == "__main__":
     unittest.main()
