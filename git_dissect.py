01b1c15d2e65041e11dcd6960c3614278ea9426a
Brendan Jackman <bhenryj0117@gmail.com>
2023-07-23 16:55:08 +0200

    magic smoke in the box

--- git_dissect.py
+++ git_dissect.py
@@ -8,7 +8,16 @@ git bisect good $GOOD
 git-dissect [--[no-]worktrees] $*
 """
 
+import dataclasses
+import logging
+import os
+import signal
 import subprocess
+import tempfile
+import threading
+import queue
+
+logger = logging.getLogger(__name__)
 
 class NotBisectingError(Exception):
     pass
@@ -25,12 +34,140 @@ def run_cmd(args: list[str]) -> str:
             f'Stderr:\n{result.stderr.decode()}')
     return result.stdout.decode()
 
-def bisect_revlist() -> list[str]:
+def good_refs() -> list[str]:
     all_refs = (l.split(" ")[1] for l in run_cmd(["git", "show-ref"]).splitlines())
-    good_refs = (r for r in all_refs if r.startswith("refs/bisect/good"))
-    args =  ["git", "rev-list", "--bisect-all", "refs/bisect/bad"]
-    args += ["^" + r for r in good_refs]
-    return [l.split(" ")[0] for l in run_cmd(args).splitlines()]
+    return [r for r in all_refs if r.startswith("refs/bisect/good")]
+
+def rev_list(include: list[str], exclude: list[str], extra_args=[]) -> list[str]:
+    args =  ["git", "rev-list"] + include + ["^" + r for r in exclude]
+    out = run_cmd(args)
+    return [l.split(" ")[0] for l in out.splitlines()]
+
+
+class WorkerPool:
+    # It's not safe to call this class' methods concurrently.
+
+    # Note: someone who knows more about Python and the GIL might be able to
+    # determine that some or all of the locking in this class is pointless.
+
+    def __init__(self, test_cmd, workdirs):
+        self._cond = threading.Condition()
+        self._rev_q = []
+        self._result_q = queue.Queue()
+        self._threads = []
+        self._subprocesses = {}
+        self._canceled = set()
+        self._done = False
+        self._test_cmd = test_cmd
+
+        # TODO: there's a bug leading to the same workdir getting reused by multiple threads.
+        for workdir in workdirs:
+            t = threading.Thread(target=self._work, args=(workdir,))
+            t.start()
+            self._threads.append(t)
+
+    def enqueue(self, rev):
+        if rev in self._canceled:
+            logger.info(f"Ignoring enqueue for canceled revision {rev}")
+            return
+
+        with self._cond:
+            self._rev_q.append(rev)
+            logger.info(f"Enqueued {rev}, new queue depth {len(self._rev_q)}")
+            self._cond.notify()
+
+    def cancel(self, rev):
+        with self._cond:
+            if rev in self._subprocesses:
+                # TODO: Does this work on Windows?
+                self._subprocesses[rev].send_signal(signal.SIGINT)
+            self._canceled.add(rev)
+
+    def _work(self, workdir):
+        while True:
+            with self._cond:
+                while not self._rev_q and not self._done:
+                    self._cond.wait()
+                if self._done:
+                    return
+
+                rev = self._rev_q[0]
+                self._rev_q = self._rev_q[1:]
+                logger.info(f"Worker in {workdir} dequeued {rev}, " +
+                            f"new queue depth {len(self._rev_q)}")
+
+                if rev in self._canceled:
+                    logger.info(f"Worker in {workdir} ignoring {rev}, already canceled")
+                    continue
+
+                # TODO: Capture stdout and stderr somewhere useful.
+                run_cmd(["git", "-C", workdir, "checkout", rev])
+                try:
+                    p = subprocess.Popen(
+                        self._test_cmd, cwd=workdir,
+                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                except (PermissionError, FileNotFoundError) as e:
+                    logging.info(f"Error running at {rev} ({e}), returning code 1")
+                    self._result_q.put((rev, 1))
+                    continue
+
+                self._subprocesses[rev] = p
+
+            p.wait()
+            logger.info(f"Worker in {workdir} got result {p.returncode} for {rev}")
+            self._result_q.put((rev, p.returncode))
+
+    def wait(self):
+        while True:
+            rev, result = self._result_q.get()
+            if rev not in self._canceled:
+                return rev, result
+
+    def interrupt_and_join(self):
+        with self._cond:
+            for p in self._subprocesses.values():
+                p.send_signal(signal.SIGINT)
+            self._done = True
+            self._cond.notify_all()
+        for t in self._threads:
+            t.join()
+
+def do_dissect(args, pool):
+    while True:
+        # --revlist-all: Get hashes for bisection in order of precedence.
+        # Returns bad as last entry
+        revs = rev_list(include=["refs/bisect/bad"], exclude=good_refs(), extra_args="--bisect-all")
+        if not revs:
+            raise RuntimeError("Bug: found no revisions in bisect range")
+        if len(revs) == 1:
+            logger.info(f"Found single revision {revs[0]}, done")
+            return revs[0]
+
+        logger.info(f"Found {len(revs) - 1} remaining revisions to test")
+
+        for rev in revs[1:]: # Drop "bad" commit
+            pool.enqueue(rev)
+
+        result_commit, returncode = pool.wait()
+        if returncode == 0:
+            run_cmd(["git", "bisect", "good", result_commit])
+            cancel_from = good_refs()
+            cancel_to = [result_commit]
+        else:
+            run_cmd(["git", "bisect", "bad", result_commit])
+            cancel_from = [result_commit]
+            cancel_to = ["refs/bisect/bad"]
+        logger.info("%s result was %d, cancelling %s to %s", result_commit, returncode,
+                    cancel_from, cancel_to)
+
+        # TODO: Here we're iterating on the range of all possible cancelable
+        # commits; there might be a lot so it's probably faster (in the
+        # worst case) to instead iterate on the enqueued commits and check
+        # if each one is in the cancelable range.
+        for commit in rev_list(include=cancel_to, exclude=cancel_from):
+            pool.cancel(commit)
+
+    return revs[0]
 
 def dissect(args):
     try:
@@ -38,10 +175,25 @@ def dissect(args):
     except CalledProcessError:
         raise NotBisectingError("Couldn't run 'git bisect log' - did you run 'git bisect'?")
 
-    revs = bisect_revlist()
-    if len(revs) == 1:
-        return revs[0]
-    print(revs)
+    num_threads = 8 # TODO
+    tmpdir = tempfile.mkdtemp()
+    worktrees = [os.path.join(tmpdir, f"worktree-{i}") for i in range(num_threads)]
+    pool = None
+    try:
+        # TODO: add option to skip worktrees
+        for w in worktrees:
+            run_cmd(["git", "worktree", "add", w, "HEAD"])
+        pool = WorkerPool(args, worktrees)
+        return do_dissect(args, pool)
+    finally:
+        for w in worktrees:
+            run_cmd(["git", "worktree", "remove", w])
+        if pool:
+            pool.interrupt_and_join()
 
 if __name__ == "__main__":
     print(run_cmd(["cat", "/etc/shadow"]))
+
+# TODO
+#   capture output
+#   option to test based on tree? Worth checking if reverts result in the same tree.
--- git_dissect_test.py
+++ git_dissect_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import logging
 import unittest
 import os
 import shutil
@@ -11,6 +12,8 @@ import git_dissect
 
 class TestGitDissect(unittest.TestCase):
     def setUp(self):
+        self.logger = logging.getLogger(self.id())
+
         tmpdir = tempfile.mkdtemp()
         self.addCleanup(lambda: shutil.rmtree(tmpdir))
         os.chdir(tmpdir)
@@ -28,12 +31,13 @@ class TestGitDissect(unittest.TestCase):
         return res.stdout.decode().strip()
 
     def commit(self, msg="dummy commit message"):
-        self.git("commit", "-m", msg)
+        self.git("commit", "--allow-empty", "-m", msg)
         return self.git("rev-parse", "HEAD")
 
-    def write(self, path, content):
+    def write_executable(self, path, content):
         with open(path, "w") as f:
             f.write(content)
+        os.chmod(path, 0o777)
         self.git("add", path)
 
     def test_not_bisect(self):
@@ -41,25 +45,50 @@ class TestGitDissect(unittest.TestCase):
             git_dissect.dissect([])
 
     def test_no_tests_needed(self):
-        self.write("run.sh", "true")
+        # TODO: Actually we should check that the script doesn't get run.
+        self.write_executable("run.sh", "true")
         good = self.commit()
-        self.write("run.sh", "exit 1")
+        self.write_executable("run.sh", "exit 1")
+        bad = self.commit()
+
+        self.git("bisect", "start")
+        self.git("bisect", "good", good)
+        self.git("bisect", "bad", bad)
+
+        self.assertEqual(git_dissect.dissect(["run.sh"]), bad, f"good: {good} bad: {bad}")
+
+    def test_smoke(self):
+        self.write_executable("run.sh", "true")
+        good = self.commit()
+        self.commit()
+        self.write_executable("run.sh", "exit 1")
+        want = self.commit()
         bad = self.commit()
 
         self.git("bisect", "start")
         self.git("bisect", "good", good)
         self.git("biset", "bad", bad)
 
-        self.assertEqual(git_dissect.dissect(["run.sh"]), bad, f"good: {good} bad: {bad}")
+        self.logger.info(self.git("log", "--graph", "--all", "--format=%H %d"))
+
+        self.assertEqual(git_dissect.dissect(["sh", "./run.sh"]), want, f"good: {good} bad: {bad}")
 
     # TODO:
     #  linear case
     #  nonlinear, multiple good
     #  nonlinear, single good
-    #  worktree mode
+    #  worktree mode (check run in expected dir)
     #  non-worktree mode
     #  replacing args
+    #  ensuring test cleanups happen
+    #  gathering output
+    #  bisect should get reset afterwardsj
 
 
 if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stderr)
+    handler.setLevel(logging.DEBUG)
+    logging.getLogger().addHandler(handler)
+
     unittest.main()
